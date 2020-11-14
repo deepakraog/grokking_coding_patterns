2e1f9de023832af87605d120006019c7f6b25746
deepakraog <gaikwad.dcg@gmail.com>
2020-11-14 17:02:20 +0530

    comments added

--- src/drg/grokking/slidingwindow/AverageOfSubarrayOfSizeK.java
+++ src/drg/grokking/slidingwindow/AverageOfSubarrayOfSizeK.java
@@ -8,28 +8,31 @@ public class AverageOfSubarrayOfSizeK {
 	public static double[] findAverages(int k, int[] arr) {
 		double result[] = new double[(arr.length - k) + 1];
 		for (int i = 0; i <= arr.length - k; i++) {
+			// find sum of next 'K' elements
 			double sum = 0;
 			for (int j = i; j < k + i; j++) {
 				sum += arr[j];
 			}
-			result[i] = sum / k;
+			result[i] = sum / k; // calculate average
 		}
 
 		return result;
 	}
-
+	
+	// Time Complexity O(n)
 	public static double[] findAveragesSlidWind(int k, int[] arr) {
 		double result[] = new double[(arr.length - k) + 1];
 		double windowSum = 0;
 		int windowStart = 0;
 
 		for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-			windowSum += arr[windowEnd];
+			windowSum += arr[windowEnd]; // add the next element
 
+			// slide the window, we don't need to slide if we've not hit the required window size of 'k'
 			if (windowEnd >= k - 1) {
-				result[windowStart] = windowSum / k;
-				windowSum -= arr[windowStart];
-				windowStart++;
+				result[windowStart] = windowSum / k; // calculate the average
+				windowSum -= arr[windowStart]; // subtract the element going out
+				windowStart++; // slide the window ahead
 			}
 		}
 
